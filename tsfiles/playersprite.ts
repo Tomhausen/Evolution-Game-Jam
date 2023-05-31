c99a0a4868fb791a78987d5152d337e458e1ed71
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-05-31 21:24:07 +0100

    attack facing up asset added

--- tsfiles/playersprite.ts
+++ tsfiles/playersprite.ts
@@ -1,17 +1,29 @@
+enum Directions {
+    UP = 0,
+    DOWN = 1,
+    LEFT = 2,
+    RIGHT = 3
+}
+
 class PlayerSprite extends BaseSprite{
 
     private speed = 100;
-    private isAttack = false;
+    private isAttacking = false;
     private isWalking = false;
+    private direction = Directions.DOWN;
     
     constructor(){
-        super(sprites.castle.heroFrontAttack1, SpriteKind.Player);
+        super(sprites.castle.heroWalkFront1, SpriteKind.Player);
         this.initialiseControls();
     }
 
     private initialiseControls(){
         controller.A.onEvent(ControllerButtonEvent.Pressed, function attack() {
-            animation.runImageAnimation(this.sprite, assets.animation`heroFrontAttack`, 100, false);
+            let frameLen = 100
+            let attackLen = assets.animation`heroFrontAttack`.length
+            animation.runImageAnimation(this.sprite, assets.animation`heroFrontAttack`, frameLen, false);
+            this.isAttacking = true;
+            timer.after(frameLen * attackLen, function stopAttacking(){this.isAttacking = false})
         })
     }
 }
